f4095419972cea5a1bd62ca7dd0cea56d9d2f8ec
Amaury Dupuich <amaury.dupuich@live.fr>
2021-07-09 11:06:18 +0200

    Appel de methode GetSymptoms et du Constructeur ReadSymptomDataFromFile pour commencer à rendre la la fonction main plus porpre, supression de lignes de codes obsolètes sysout et reader

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,40 +1,66 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.List;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0; // initialize to 0
+	private static int headCount = 0; // initialize to 0
 	private static int rashCount = 0; // initialize to 0
 	private static int pupilCount = 0; // initialize to 0
 
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader(
-				new FileReader("C:\\Users\\Amaur\\git\\ocr_projet4\\Project02Eclipse\\symptoms.txt"));
-		String line = reader.readLine();
 
-		int headCount = 0; // counts headaches
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			} else if (line.equals("rush")) {
-				rashCount++;
-			} else if (line.contains("pupils")) {
-				pupilCount++;
+		/*
+		 * Ce code est mis de côté Try to call the method BufferedReader reader = new
+		 * BufferedReader( new FileReader(
+		 * "C:\\Users\\Amaur\\git\\ocr_projet4\\Project02Eclipse\\symptoms.txt"));
+		 * String line = reader.readLine(); System.out.println(reader);
+		 */
+
+		/**
+		 * first get input with the ReadSymptomDataFromFile class, readSymptoms est une
+		 * nouvelle instance de la classe ReadSymptomDataFromFile prenant le filepath
+		 * comme argument
+		 */
+		ReadSymptomDataFromFile readSymptoms = new ReadSymptomDataFromFile(
+				"C:\\Users\\Amaur\\git\\ocr_projet4\\Project02Eclipse\\symptoms.txt");
+		/**
+		 * Initie nameOfSymptoms, instancié par la methode Getsymptoms pointant sur
+		 * readSymptoms
+		 */
+		List<String> nameOfSymptoms = readSymptoms.GetSymptoms();
+
+		/*
+		 * System.out.println(nameOfSymptoms); Ce code ne sert plus à grand chose
+		 *
+		 * counts headaches : adaptons le code pour compter les symptoms, initialement
+		 * proposé dans le code
+		 */
+		for (String line : nameOfSymptoms) {
+			/*
+			 * J'opte pour une boucle for plus que while, pour chaque occurence
+			 *
+			 * while (line != null && !nameOfSymptoms.isEmpty()) Ce code ne sert plus à
+			 * grand chose
+			 */
+			{
+				System.out.println("symptom from file: " + line);
+				if (line.equals("headache")) {
+					headCount++;
+					System.out.println("number of headaches: " + headCount);
+				} else if (line.equals("rash")) {
+					rashCount++;
+				} else if (line.contains("pupils")) {
+					pupilCount++;
+				}
 			}
-
-			line = reader.readLine(); // get another symptom
 		}
-
-		// next generate output
+		/* line = reader.readLine(); ce code ne sert plus next generate output */
 		FileWriter writer = new FileWriter("result.out");
-		writer.write("headache: " + headacheCount + "\n");
+		writer.write("headache: " + headCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.write("dialated pupils: " + pupilCount);
 		writer.close();
+		// reader.close(); Ce code ne sert plus
 	}
 }
