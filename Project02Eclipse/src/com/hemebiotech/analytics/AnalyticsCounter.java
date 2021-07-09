285c55054711dc9911414bcd5b73e6cb93a7d8ae
Amaury Dupuich <amaury.dupuich@live.fr>
2021-07-09 15:59:02 +0200

    J'ai modifié la manière de compter les itérations des symptoms afin de tenir compte d'ajouts utltérieurs de données dans le fichier texte. De même j'ai modifié le writer qui désormais écrira tous les symptomes lus dans le fichier texte.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -2,65 +2,81 @@ package com.hemebiotech.analytics;
 
 import java.io.FileWriter;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headCount = 0; // initialize to 0
-	private static int rashCount = 0; // initialize to 0
-	private static int pupilCount = 0; // initialize to 0
+	/*
+	 * private static int headCount = 0; // initialize to 0 private static int
+	 * rashCount = 0; // initialize to 0 private static int pupilCount = 0; //
+	 * initialize to 0
+	 */
 
 	public static void main(String args[]) throws Exception {
 
-		/*
-		 * Ce code est mis de côté Try to call the method BufferedReader reader = new
-		 * BufferedReader( new FileReader(
-		 * "C:\\Users\\Amaur\\git\\ocr_projet4\\Project02Eclipse\\symptoms.txt"));
-		 * String line = reader.readLine(); System.out.println(reader);
-		 */
-
 		/**
-		 * first get input with the ReadSymptomDataFromFile class, readSymptoms est une
-		 * nouvelle instance de la classe ReadSymptomDataFromFile prenant le filepath
-		 * comme argument
+		 * first get input with the ReadSymptomDataFromFile constructor, readSymptoms is
+		 * a new instance of ReadSymptomDataFromFile with filepath as argument
 		 */
 		ReadSymptomDataFromFile readSymptoms = new ReadSymptomDataFromFile(
 				"C:\\Users\\Amaur\\git\\ocr_projet4\\Project02Eclipse\\symptoms.txt");
 		/**
-		 * Initie nameOfSymptoms, instancié par la methode Getsymptoms pointant sur
-		 * readSymptoms
+		 * Initialyse nameOfSymptoms with method Getsymptoms on readSymptoms
 		 */
 		List<String> nameOfSymptoms = readSymptoms.GetSymptoms();
 
 		/*
-		 * System.out.println(nameOfSymptoms); Ce code ne sert plus à grand chose
-		 *
-		 * counts headaches : adaptons le code pour compter les symptoms, initialement
-		 * proposé dans le code
+		 * Je vais avoir besoin d'"un tableau type dictionnaire qui aura pour clé les
+		 * symptoms et pour valeur les occurences, j'utilise le constructeur TreeMap car
+		 * il itère en suivant l'odre des données reçues HashMap ne garanti pas d'itérer
+		 * dans le même ordre avec l'ajout de données et le TreeMap met dans l'odre
+		 * alpha ce qui n'est pas le cas du Hashmap
 		 */
+		Map<String, Integer> symptomsCount = new TreeMap<>();
+		// pour remplir ce tableau je garde une boucle for
 		for (String line : nameOfSymptoms) {
 			/*
-			 * J'opte pour une boucle for plus que while, pour chaque occurence
-			 *
-			 * while (line != null && !nameOfSymptoms.isEmpty()) Ce code ne sert plus à
-			 * grand chose
+			 * je lui demande d'ajouter d'ajouter la clé line, avec la valeur : tester
+			 * l'objet line de symptomsCount, si vrai +1 : sinon 1.
 			 */
-			{
-				System.out.println("symptom from file: " + line);
-				if (line.equals("headache")) {
-					headCount++;
-					System.out.println("number of headaches: " + headCount);
-				} else if (line.equals("rash")) {
-					rashCount++;
-				} else if (line.contains("pupils")) {
-					pupilCount++;
-				}
-			}
+			symptomsCount.put(line, symptomsCount.containsKey(line) ? symptomsCount.get(line) + 1 : 1);
 		}
-		/* line = reader.readLine(); ce code ne sert plus next generate output */
+		// System.out.println(symptomsCount); // permet de voir le résultat
+		/*
+		 * Ce bloc de code est mis de côté { System.out.println("symptom from file: " +
+		 * line); if (line.equals("headache")) { headCount++;
+		 * System.out.println("number of headaches: " + headCount); } else if
+		 * (line.equals("rash")) { rashCount++; } else if (line.contains("pupils")) {
+		 * pupilCount++; } }
+		 */
+		/**
+		 * In order to have the result in file we create writer with class Filewriter
+		 */
 		FileWriter writer = new FileWriter("result.out");
-		writer.write("headache: " + headCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount);
+		/*
+		 * Ce code est mis de côté pour trouver une autre solution
+		 * writer.write("headache: " + headCount + "\n"); writer.write("rash: " +
+		 * rashCount + "\n"); writer.write("dialated pupils: " + pupilCount);
+		 * writer.close();
+		 */
+		for (String line : symptomsCount.keySet()) {
+			writer.write(line + " : " + symptomsCount.get(line) + "\n");
+		}
+		// String symptom;
+		// Integer valeur;
+		/**
+		 * I use Interface Iterator beacause I need to repeat an action for each line
+		 */
+		/*
+		 * Iterator<String> iterator = symptomsCount.keySet().iterator(); // String
+		 * newligne = System.getProperty("line.separator"); while (iterator.hasNext()) {
+		 * symptom = iterator.next(); valeur = symptomsCount.get(symptom);
+		 * writer.write(symptom + " : " + valeur + "\n"); }
+		 */
+
 		writer.close();
-		// reader.close(); Ce code ne sert plus
 	}
 }
+
+//}
+//}
